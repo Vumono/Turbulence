8582c5b84a316d197428e458c9d422a684940f53
Vumono <87247208+Vumono@users.noreply.github.com>
2021-07-15 14:05:38 +0200

    Update client.lua
    Thanks to MacHunt the script works now

--- gb-turbulence/client.lua
+++ gb-turbulence/client.lua
@@ -1,78 +1,69 @@
-ESX = nil
-
-TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-
-RegisterNetEvent('esx:playerLoaded')
-AddEventHandler('esx:playerLoaded', function(xPlayer)
-    ESX.PlayerData = xPlayer
-    TriggerEvent('esx_license:getLicenses')
-end)
-
+ESX                             = nil
 
 Citizen.CreateThread(function()
-while true do
-    Citizen.Wait(1000)
-    local src = source
-    local xPlayer = ESX.GetPlayerFromId(src)
-    local identifier = GetPlayerIdentifiers(source)
-    local ped = GetPlayerPed(-1)
-    local heli = GetVehiclePedIsIn(ped, false)
-    local chance = math.random(1,100)
-    local model = GetEntityModel(heli)
-    if IsThisModelAHeli(model) then
-        MySQL.Async.fetchAll('SELECT type FROM user_licenses WHERE identifier = @owner', {["@owner"] = xPlayer.identifier},
-            function (licenses)
-                if type ~= nil then
-                    for i=1, #licenses, 1 do
-                        if type == 'heli' and IsPedInAnyHeli(ped) then
-                            if licenses[i].type == 'heli' then
-                                SetHeliTurbulenceScalar(heli, 0.0)
-                                SetHelicopterRollPitchYawMult(heli, 0.0)
-                            end
-                        else
-                            SetHelicopterRollPitchYawMult(heli, 0.4)
-                            SetHeliTurbulenceScalar(heli, 0.7)
-                            SetVehicleGeneratesEngineShockingEvents(heli, CEventShockingHelicopterOverhead)
-                            if chance then
-                                SetVehicleEngineOn(heli, false, true, false)
-                            end
-                        end
-                    end 
-                end 
-            end)
-        end
-    end
+	while ESX == nil do
+		TriggerEvent('esx:getSharedObject', function(obj) 
+			ESX = obj 
+		end)
+		Citizen.Wait(0)
+	end
 end)
 
+local oldveh = nil
+local turbulence = 0.7
+local roll = 0.4
+
+local failureChance = 5 --in %
+
+local license = {
+	plane = 'plane',
+	heli = 'heli'
+}
+
+local inAircraft = false
+
 Citizen.CreateThread(function()
-while true do
-    Citizen.Wait(1000)
-    local src = source
-    local xPlayer = ESX.GetPlayerFromId(src)
-    local identifier = GetPlayerIdentifiers(source)
-    local ped = GetPlayerPed(-1)
-    local heli = GetVehiclePedIsIn(ped, false)
-    local chance = math.random(1,100)
-    local model = GetEntityModel(heli)
-    if IsThisModelAPlane(model) then
-        MySQL.Async.fetchAll('SELECT type FROM user_licenses WHERE identifier = @owner', {["@owner"] = xPlayer.identifier},
-            function (licenses)
-                if type ~= nil then
-                    for i=1, #licenses, 1 do
-                        if type == 'plane' and IsPedInAnyPlane(ped) then
-                            if licenses[i].type == 'plane' then
-                                SetPlaneTurbulenceMultiplier(heli, 0.0)
-                            end
-                        else
-                            SetPlaneTurbulenceMultiplier(heli, 0.6)
-                            SetVehicleGeneratesEngineShockingEvents(heli, CEventShockingHelicopterOverhead)
-                            if chance then
-                                SetVehicleEngineOn(heli, false, true, false)
-                            end
-                        end
-                    end 
-                end 
-            end)
-        end
-    end
+	while true do
+		Citizen.Wait(1000)
+		local ped = GetPlayerPed(-1)
+		if IsPedInAnyVehicle(ped, false) then
+			local veh = GetVehiclePedIsIn(ped, false)
+			if oldveh == nil or veh ~= oldveh then
+				oldveh = veh
+				if IsPedInAnyHeli(ped) then
+					inAircraft = true
+					ESX.TriggerServerCallback('esx_license:checkLicense', function(hasLicense)
+						if hasLicense then
+							SetHeliTurbulenceScalar(veh, 0.0)
+							SetHelicopterRollPitchYawMult(veh, 0.0)
+						else
+							SetHelicopterRollPitchYawMult(veh, turbulence)
+							SetHeliTurbulenceScalar(veh, roll)
+							--SetVehicleGeneratesEngineShockingEvents(veh, CEventShockingHelicopterOverhead) --param not a string thus undefined
+						end
+					end, GetPlayerServerId(ped), license.heli)
+				elseif IsPedInAnyPlane(ped) then
+					inAircraft = true
+					ESX.TriggerServerCallback('esx_license:checkLicense', function(hasLicense)
+						if hasLicense then
+							SetPlaneTurbulenceMultiplier(veh, 0.0)
+						else
+							SetPlaneTurbulenceMultiplier(veh, turbulence)
+							--SetVehicleGeneratesEngineShockingEvents(veh, CEventShockingHelicopterOverhead) --param not a string thus undefined
+						end
+					end, GetPlayerServerId(ped), license.plane)
+				else
+					inAircraft = false
+				end
+			end
+			if inAircraft then
+				local chance = math.random(100)
+				if chance >= failureChance then
+					SetVehicleEngineOn(veh, false, true, false)
+				end
+			end
+		else
+			inAircraft = false
+		end
+	end
 end)
